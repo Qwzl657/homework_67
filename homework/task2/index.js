e661b54f6b70713b123d79ce4a02585e6aa72b05
Alibek <sultanovalibek2020@gmail.com>
2026-05-14 17:34:39 +0600

    Добавил  загрузить список покемонов через AJAX и вывести ссылками на странице

---
+++ homework/task2/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+window.onload = function () {
+
+    const pokemonList = document.getElementById('pokemon-list');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        const data = JSON.parse(this.response);
+        const pokemons = data.results;
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+
+            const link = document.createElement('a');
+            link.href = 'pokemon.html?name=' + pokemon.name;
+            link.innerHTML = pokemon.name;
+
+            const div = document.createElement('div');
+            div.append(link);
+
+            pokemonList.append(div);
+        }
+    };
+
+    xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon');
+    xhr.send();
+
+};
