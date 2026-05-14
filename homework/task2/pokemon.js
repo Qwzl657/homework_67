80e9d014bfab6414a24e069e02fc59dabf3116d0
Alibek <sultanovalibek2020@gmail.com>
2026-05-14 17:37:17 +0600

    Добавил данные покемона через AJAX и вывести имя, вес, способности и типы/HTML разметку для детальной страницы покемона

---
+++ homework/task2/pokemon.js
@@ -0,0 +1,47 @@
+'use strict';
+
+window.onload = function () {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const pokemonName = urlParams.get('name');
+
+    const detail = document.getElementById('pokemon-detail');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        const data = JSON.parse(this.response);
+
+        const title = document.createElement('h1');
+        title.innerHTML = data.name;
+        detail.append(title);
+
+        const weight = document.createElement('p');
+        weight.innerHTML = 'Вес: ' + data.weight;
+        detail.append(weight);
+
+        const abilitiesTitle = document.createElement('h2');
+        abilitiesTitle.innerHTML = 'Способности:';
+        detail.append(abilitiesTitle);
+
+        for (let i = 0; i < data.abilities.length; i++) {
+            const ability = document.createElement('p');
+            ability.innerHTML = data.abilities[i].ability.name;
+            detail.append(ability);
+        }
+
+        const typesTitle = document.createElement('h2');
+        typesTitle.innerHTML = 'Типы:';
+        detail.append(typesTitle);
+
+        for (let i = 0; i < data.types.length; i++) {
+            const type = document.createElement('p');
+            type.innerHTML = data.types[i].type.name;
+            detail.append(type);
+        }
+    };
+
+    xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + pokemonName);
+    xhr.send();
+
+};
