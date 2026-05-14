e248c1a2562d7113ecfe6ef7e1b606e672186990
Alibek <sultanovalibek2020@gmail.com>
2026-05-14 17:32:45 +0600

    Добавил в task1.js window.onload чтобы браузер читал файл сверхну вниз

--- homework/task1/task1.js
+++ homework/task1/task1.js
@@ -1,23 +1,13 @@
 'use strict';
 
-const addBtn = document.getElementById('add-item-btn');
-const wrapper = document.getElementById('notifications-wrapper');
-
-addBtn.addEventListener('click', function () {
-
-    const notification = document.createElement('div');
-    notification.classList.add('notification');
-    notification.innerHTML = 'У вас новое уведомление!';
-
-    const closeBtn = document.createElement('button');
-    closeBtn.innerHTML = 'Закрыть';// task1.js
-    'use strict';
+window.onload = function () {
 
     const addBtn = document.getElementById('add-item-btn');
     const wrapper = document.getElementById('notifications-wrapper');
 
     addBtn.addEventListener('click', function () {
 
+        // Создаём div-уведомление
         const notification = document.createElement('div');
         notification.classList.add('notification');
         notification.innerHTML = 'У вас новое уведомление!';
@@ -39,16 +29,4 @@ addBtn.addEventListener('click', function () {
 
     });
 
-    closeBtn.addEventListener('click', function () {
-        notification.remove();
-    });
-
-    notification.append(closeBtn);
-
-    wrapper.append(notification);
-
-    setTimeout(function () {
-        notification.remove();
-    }, 5000);
-
-});
+};
